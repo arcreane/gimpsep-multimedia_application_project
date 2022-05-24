7dc177457cfdb077f9ad31915a9d1d8628c802e0
Yann Spanier <42733730+MeaNoCulpa@users.noreply.github.com>
2022-05-24 21:25:56 +0200

    Add files via upload
    Brightness and Resize functions (need to improve annotations / code readability, as well as add instructions regarding Resize's controls)

---
+++ Resize/Resize.h
@@ -0,0 +1,13 @@
+#ifndef RESIZE_H 
+#define RESIZE_H 
+
+//func or class
+
+	static void scale_on_trackbar();
+	static void create_scale_window();
+	static void create_dimensional_window();
+	static int initialize();
+	
+
+
+#endif
